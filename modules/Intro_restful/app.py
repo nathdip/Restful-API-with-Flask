f4d184d7d511ad8369d43791017a555ba47f76a5
Dipankar Nath <dipankar.nath@bt.com>
2020-01-18 14:26:30 +0100

    Additional methods implemented

--- modules/Intro_restful/app.py
+++ modules/Intro_restful/app.py
@@ -2,7 +2,7 @@
 Initial test of a RESTful API
 Dipankar Nath
 """
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
@@ -19,14 +19,25 @@ class Item(Resource):
         return {"Item": None}, 404
 
     def post(self, name):
-        item = {'name': name, 'price': 12.00}
+        data = request.get_json()
+        price = data['price']
+        item = {'name': name, 'price': price}
         items.append(item)
         return item, 201
 
+    # def put(self, name):
+    #     raw_data = request.json()
+    #     price = raw_data['price']
+    #     for item in items:
+    #         if item['name'] == name:
+    #             item['price'] = price
+    #         else:
+    #             items.append
 
-class Itemlist(Resourse):
+
+class Itemlist(Resource):
     def get(self):
-        return {'items': items}
+        return {'items': items}, 201
 
 
 api.add_resource(Itemlist, '/items')
